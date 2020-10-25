f81101a5689b7dd0891ea7cdf052a2007e2074b8
Megan Muirhead <meganmuirhead@MacBook-Pro-2.local>
2020-10-24 21:32:40 -0600

    working on first unit test

--- projects/khaki-statistics/src/lib/components/per-department-graph/per-department-graph.component.spec.ts
+++ projects/khaki-statistics/src/lib/components/per-department-graph/per-department-graph.component.spec.ts
@@ -1,14 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PerDepartmentGraphComponent } from './per-department-graph.component';
+import {StatisticsServiceService} from "../../services/statistics-service.service";
 
 describe('PerDepartmentGraphComponent', () => {
   let component: PerDepartmentGraphComponent;
   let fixture: ComponentFixture<PerDepartmentGraphComponent>;
+  let mockStatService: Partial<StatisticsServiceService>
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PerDepartmentGraphComponent ]
+      declarations: [ PerDepartmentGraphComponent ],
+      imports: [],
+      providers: [
+        {provide: StatisticsServiceService, useValue: mockStatService}
+
+      ]
     })
     .compileComponents();
   });
