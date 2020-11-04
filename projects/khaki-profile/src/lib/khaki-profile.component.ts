68b6c19103ed666b022aff6f8ee2784f95a9478d
Jacob Boswell <jacob@s56.net>
2020-11-04 16:48:47 -0700

    added new profile project

---
+++ projects/khaki-profile/src/lib/khaki-profile.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-khaki-profile',
+  template: `
+    <p>
+      khaki-profile works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class KhakiProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ projects/khaki-profile/src/lib/khaki-profile.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { KhakiProfileComponent } from './khaki-profile.component';
+
+
+
+@NgModule({
+  declarations: [KhakiProfileComponent],
+  imports: [
+  ],
+  exports: [KhakiProfileComponent]
+})
+export class KhakiProfileModule { }
