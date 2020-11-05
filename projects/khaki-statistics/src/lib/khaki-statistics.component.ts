9c812a849075b739f7a66cbe2d3246529e1078f1
Megan Muirhead <meganmuirhead@MacBook-Pro-2.local>
2020-11-04 19:11:40 -0700

    fixing background

--- projects/khaki-statistics/src/lib/khaki-statistics.component.ts
+++ projects/khaki-statistics/src/lib/khaki-statistics.component.ts
@@ -26,12 +26,36 @@ import {SpinnerFacadeService} from './state/facades/spinner-facade.service';
       </div>
       <div class="bottom-div"></div>
     </div>
+<!--<div class="padding">-->
+
+<!--  <div>-->
+<!--    <lib-time-based-stat-summary></lib-time-based-stat-summary>-->
+<!--  </div>-->
+
+<!--  <div class="space-between white-container">-->
+
+<!--    <div>-->
+<!--      <lib-twelve-month-trailing-graph></lib-twelve-month-trailing-graph>-->
+
+<!--    </div>-->
+<!--    <div>-->
+<!--      <lib-per-department-graph></lib-per-department-graph>-->
+
+<!--    </div>-->
+<!--  </div>-->
+<!--  <div class="white-container around-twelve">-->
+<!--    <lib-organizers-table></lib-organizers-table>-->
+
+
+<!--  </div>-->
+<!--  <div class="bottom-div"></div>-->
+<!--</div>-->
   `,
   styles: [
       '.space-between { display: flex; justify-content: space-around}',
-    '.padding { padding: 30px 50px ; background-color: #e3eef6}',
+    '.padding { padding: 30px 50px ; background-color: #EEF8FF}',
     '.white-container { background-color: white; padding:30px}',
-    '.bottom-div { background-color: white; padding: 10px; height: 400px; width: 100%;}',
+    '.bottom-div { background-color: white; padding: 10px; height: 400px;}',
   ]
 })
 export class KhakiStatisticsComponent implements OnInit {
