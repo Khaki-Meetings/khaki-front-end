eba8f4b23bb3b23130520a6522478121bac69eca
Jacob Boswell <jacob@s56.net>
2020-11-11 16:44:18 -0700

    setup structure

--- projects/khaki-profile/src/lib/khaki-profile-routing.module.ts
+++ projects/khaki-profile/src/lib/khaki-profile-routing.module.ts
@@ -1,12 +1,40 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {KhakiProfileComponent} from './khaki-profile.component';
+import {ProfileNameComponent} from './components/profile-name/profile-name.component';
+import {ProfileEmailComponent} from './components/profile-email/profile-email.component';
+import {ProfileNotificationsComponent} from './components/profile-notifications/profile-notifications.component';
+import {ProfileHelpComponent} from './components/profile-help/profile-help.component';
+import {ProfileLogoutComponent} from './components/profile-logout/profile-logout.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: KhakiProfileComponent
-  }
+    component: KhakiProfileComponent,
+    children: [
+      {
+        path: 'name',
+        component: ProfileNameComponent
+      },
+      {
+        path: 'email',
+        component: ProfileEmailComponent
+      },
+      {
+        path: 'notifications',
+        component: ProfileNotificationsComponent
+      },
+      {
+        path: 'help',
+        component: ProfileHelpComponent
+      },
+      {
+        path: 'logout',
+        component: ProfileLogoutComponent
+      }
+    ]
+  },
+
 ];
 
 @NgModule({
