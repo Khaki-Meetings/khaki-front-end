f680f02e2cdcd5c7ed8bc8279b0d4e3e7cc6b47e
Jacob Boswell <jacob@s56.net>
2020-11-11 16:34:38 -0700

    starting profile

---
+++ projects/khaki-profile/src/lib/components/profile-notifications/profile-notifications.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-profile-notifications',
+  templateUrl: './profile-notifications.component.html',
+  styleUrls: ['./profile-notifications.component.scss']
+})
+export class ProfileNotificationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
