d349a1c714ea73ae2ed5228c9c9457674d8776cb
Jacob Boswell <jacob@s56.net>
2020-12-20 15:04:56 -0700

    got setting userProfile working

--- projects/khaki-settings/src/lib/components/settings-options/settings-options.component.ts
+++ projects/khaki-settings/src/lib/components/settings-options/settings-options.component.ts
@@ -2,35 +2,38 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserProfileResponseDto } from '../../services/models/userProfileResponseDto';
 import { UserProfileFacadeService } from '../../state/facades/user-profile-facade.service';
+import {HistorianService, Logging} from '@natr/historian';
 
+@Logging
 @Component({
   selector: 'lib-settings-options',
   templateUrl: './settings-options.component.html',
   styleUrls: ['./settings-options.component.scss']
 })
 export class SettingsOptionsComponent implements OnInit {
+  private logger: HistorianService;
 
-  menunow = '';
+  menuNow = '';
   userProfile: UserProfileResponseDto = {};
 
-  constructor(private router: Router, private userProfileService: UserProfileFacadeService) { }
+  constructor(private router: Router, private userProfileFacade: UserProfileFacadeService) { }
 
   ngOnInit(): void {
-    this.userProfileService.userProfile()
+    this.userProfileFacade.userProfile()
       // .pipe(tap(data => this.logger.debug('subscription', data)))
       .subscribe(userProfile => {
-        // this.logger.debug('onInit', userProfile);
+        this.logger.debug('onInit', userProfile);
         this.userProfile = userProfile;
       });
   }
 
   onMenu(e, menuname): void {
-    if (this.menunow === menuname) {
-      this.menunow = '';
+    if (this.menuNow === menuname) {
+      this.menuNow = '';
       this.router.navigateByUrl('settings');
       e.preventDefault();
     } else {
-      this.menunow = menuname;
+      this.menuNow = menuname;
     }
   }
 
