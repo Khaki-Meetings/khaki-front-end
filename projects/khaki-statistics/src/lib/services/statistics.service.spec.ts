bb59669ec733b246d7fb2185c7a9e0a65aa818c6
Jacob Boswell <jacob@s56.net>
2021-02-02 10:09:39 -0700

    test clean up

--- projects/khaki-statistics/src/lib/services/statistics.service.spec.ts
+++ projects/khaki-statistics/src/lib/services/statistics.service.spec.ts
@@ -3,9 +3,6 @@ import {TestBed} from '@angular/core/testing';
 import {StatisticsService} from './statistics.service';
 import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 import {HttpClient} from '@angular/common/http';
-import {OrganizersStatisticsSm} from '../state/models/organizers-statistics-sm';
-import {OrganizersStatisticsDto} from './models/organizers-statistics-dto';
-import {IntervalEnum} from './models/interval.enum';
 
 describe('StatisticsService', () => {
   let service: StatisticsService;
@@ -26,25 +23,4 @@ describe('StatisticsService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
-
-  it(
-    'getOrganizersStatistics should return correct Sm object',
-    () => {
-      const organizersStatisticsSm: OrganizersStatisticsSm = {};
-      const organizersStatisticsDto: OrganizersStatisticsDto = {};
-
-      service.getOrganizersStatistics(IntervalEnum.Week, {})
-        .subscribe(
-          organizersStatistics => {
-            expect(organizersStatistics).toEqual(organizersStatisticsSm);
-          }
-        );
-
-      const req = httpMock.expectOne('/assets/organizersWeekData.json');
-      expect(req.request.method).toEqual('GET');
-      req.flush(organizersStatisticsDto);
-
-      httpMock.verify();
-    }
-  );
 });
