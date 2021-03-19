41463909b36f205ff93e30ed29fb12eb51dfefc1
Jeff Keller <jeff@jeffkeller.com>
2021-03-19 15:13:00 -0400

    Deleted old khaki-meetings project

--- projects/khaki-meetings/src/lib/state/models/meetings-feature.ts
+++
@@ -1,6 +0,0 @@
-import { CalendarEventsResponseDto } from '../../services/models/calendarEventsResponseDto';
-import { calendarEventsFeatureKey } from '../reducers/calendar-events-reducer.reducer';
-
-export interface MeetingsFeature {
-  [calendarEventsFeatureKey]: CalendarEventsResponseDto;
-}
