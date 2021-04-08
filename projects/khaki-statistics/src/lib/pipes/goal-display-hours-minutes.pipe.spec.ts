450cc6d8b63c36c872690c3426ec323f80d9451e
Jeff Keller <jeff@jeffkeller.com>
2021-04-08 09:32:13 -0400

    No minutes display if time is over 1000 hours

---
+++ projects/khaki-statistics/src/lib/pipes/goal-display-hours-minutes.pipe.spec.ts
@@ -0,0 +1,9 @@
+import { GoalDisplayHoursMinutesPipe } from './goal-display-hours-minutes.pipe';
+
+
+describe('GoalDisplayHoursMinutesPipe', () => {
+  it('create an instance', () => {
+    const pipe = new GoalDisplayHoursMinutesPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
